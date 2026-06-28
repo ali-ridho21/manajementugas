905762699f3a952d75729628b9a0ebbb258866c1
ali-ridho21 <idho68480@gmail.com>
2026-06-28 16:13:08 +0700

    Update task_db.sql

--- task_db.sql
+++ task_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2026 at 09:41 AM
+-- Generation Time: Jun 28, 2026 at 11:12 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,15 +31,19 @@ CREATE TABLE `tasks` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
-  `is_completed` tinyint(1) DEFAULT NULL
+  `status` tinyint(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `title`, `description`, `is_completed`) VALUES
-(7, 'a', 'a', 1);
+INSERT INTO `tasks` (`id`, `title`, `description`, `status`, `created_at`, `updated_at`) VALUES
+(4, 'Fullstack', 'Membuat Website', 0, '2026-06-28 16:07:46', '2026-06-28 16:07:46'),
+(5, 'Frontend', 'Membuat desain tampilan web', 0, '2026-06-28 16:08:32', '2026-06-28 16:08:32'),
+(6, 'Backend', 'Logika Pemrosesan Data\nPenghubung ke Database\nPenyedia API (Application Programming Interface)\nKeamanan dan Autentikasi', 0, '2026-06-28 16:11:31', '2026-06-28 16:11:31');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +64,7 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
